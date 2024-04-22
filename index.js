6a8d6d5ea5d89cc0f4994c68c8c19682ff764159
Hamza saleem <126867941+hamzatheprogrammer@users.noreply.github.com>
2024-04-22 21:49:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function tablefinder(){
+    let result = document.getElementById('result');
+    let num = document.getElementById('num').value;
+    result.innerHTML = '';
+    for (let i = 1; i<=10; i++){
+        result.innerHTML += `${num} X ${i} = ${num*i} <br>`;
+
+    }
+}
